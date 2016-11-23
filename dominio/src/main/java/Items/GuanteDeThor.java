b1426259fe262db773fec5e8a1bcf6ad8270f770
Unknown <axeldearias@gmail.com>
2016-11-23 15:59:26 -0300

    Se agrega Item guanteDeThor

---
+++ dominio/src/main/java/Items/GuanteDeThor.java
@@ -0,0 +1,42 @@
+package Items;
+
+import Game.*;
+import StatListeners.*;
+
+public class GuanteDeThor extends ItemMano implements SpeedListener , EvasionListener,AttackListener{
+
+	public GuanteDeThor() {
+		nombre = "Guante de Thor";
+		descripcion = "Guante que dismuye el transcurso del tiempo" + "\n\n+20 Velocidad";
+	}
+
+	@Override
+	public void equipar(PC pj) {
+		pj.speedSubscribe(this);
+		pj.evasionSubscribe(this);
+		pj.attackSubscribe(this);
+	}
+
+	@Override
+	public void desequipar(PC pj) {
+		pj.speedUnsubscribe(this);
+		pj.evasionUnsubscribe(this);
+		pj.attackUnsubscribe(this);
+	}
+
+	@Override
+	public int getSpeedBonus() {
+		return 20;
+	}
+
+	@Override
+	public int getEvasionBonus() {
+		return 20;
+	}
+
+	@Override
+	public int getAttackBonus() {
+		return 5;
+	}
+
+}
